60b4def1ad752a428f06a48cf9116b2d8bfa81ff
16-Bit-Dog <67922228+16-Bit-Dog@users.noreply.github.com>
2021-12-13 03:17:48 -0500

    initial

---
+++ DX12P/DX12H.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+// Windows Runtime Library. Needed for Microsoft::WRL::ComPtr<> template class.
+#include <wrl.h>
+using namespace Microsoft::WRL;
+
+// DirectX 12 specific headers.
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+
+struct MainDX12Objects {
+	HWND hwnd;
+
+
+	void StartUpLogic() {
+
+	}
+}DXM;
---
+++ DX12P/Window.cpp
@@ -0,0 +1,39 @@
+#define GLFW_INCLUDE_NONE
+#define GLFW_EXPOSE_NATIVE_WIN32
+
+#include <iostream>
+#include <../GLFW/glfw3.h>
+#include <../GLFW/glfw3native.h>
+#include "DX12H.h"
+
+struct GLFW_Window_C{
+	GLFWwindow* window;
+}Win;
+
+int runMain() {
+	return -1;
+}
+
+int main() {
+	glfwInit();
+
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	Win.window = glfwCreateWindow(800, 800, "DX12_App", NULL, NULL);
+	
+	if (Win.window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(Win.window);
+
+	DXM.hwnd = glfwGetWin32Window(Win.window);
+	
+	DXM.StartUpLogic();
+
+	while (!glfwWindowShouldClose(Win.window)) {
+		runMain();
+	}
+}
